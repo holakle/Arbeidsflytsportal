0de0aa4e5599c385401a0a43a0331f34770bab31
Holakle <holakle@gmail.com>
2026-03-01 23:05:01 +0100

    chore: configure eslint for typescript workspaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,29 @@
-﻿module.exports = {
+module.exports = {
   root: true,
   env: {
     node: true,
-    es2022: true
+    es2022: true,
   },
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  extends: ["eslint:recommended", "prettier"],
-  ignorePatterns: ["dist", ".next", "node_modules", "coverage", "apps/api/generated"]
+  extends: ['eslint:recommended', 'prettier'],
+  ignorePatterns: ['dist', '.next', 'node_modules', 'coverage', 'apps/api/generated'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  ],
 };
-
